18c45d4495ebaedb4fe0530fdfa8c17dfd29948c
jun14111 <1411187249@qq.com>
2021-05-03 11:46:36 +0800

    新增： warmup功能 多线程训练
    改进：
    将更多的参数放入配置文件config.josn中统一管理

--- train.py
+++ train.py
@@ -10,29 +10,39 @@ from keras.optimizers import *
 from DataGenerator import DataGenerator
 from backend import NerualNetworkModel
 
-
+"""
+1. 读取配置文件
+2. 加载各个类型的配置数据
+"""
 # 读取配置文件
-config_path = r'.\config.json'
+config_path = r'./config.json'
 with open(config_path) as config_buffer:
     config = json.loads(config_buffer.read())
 
+# model
 model_data = config['model']['model_data']
 model_path = config['model']['model_path']
 
+# train
 pretrained_weights = config['train']['pretrained_weights']
 train_file = config['train']['train_data_file']
 train_dir = config['train']['train_data_folder']
 train_prob = config['train']['train_prob']
-
+train_workers = config['train']['workers']
+batch_size = config['train']['batch_size']
+warmup_epochs = config['train']['warmup_epochs']
+# valid
 valid_file = config['valid']['valid_data_file']
 valid_dir = config['valid']['valid_data_folder']
 valid_prob = config['valid']['valid_prob']
 
 
 def main(load_best_weight=False, load_pre_weight=False):
+    # 加载模型结构
     model = NerualNetworkModel().ResNet50(input_size=(40, 120, 3), regularizer=0.0001, droprate=0.25)
-    batch_size = 16
-
+    """
+    根据参数加载模型数据
+    """
     # 读取预训练权重
     if load_pre_weight:
         try:
@@ -44,7 +54,7 @@ def main(load_best_weight=False, load_pre_weight=False):
         else:
             print('pre weights has load')
 
-    #  最好的权重
+    # 最好的权重
     elif load_best_weight:
         try:
             print(model_path)
@@ -62,26 +72,42 @@ def main(load_best_weight=False, load_pre_weight=False):
                   optimizer=Adam(0.001, amsgrad=True),
                   metrics=['accuracy', my_acc])
 
+    """
+    创建数据生成器
+    """
     # 训练数据生成器
     train_data_gen = DataGenerator(data_file=train_file, data_dir=train_dir, img_shape=(120, 40), batch_size=batch_size,
                                    data_aug=True,
                                    prob=train_prob,
                                    shuffle=True)
-
     # 验证数据生成器
     valid_data_gen = DataGenerator(data_file=valid_file, data_dir=valid_dir, img_shape=(120, 40), batch_size=batch_size,
                                    data_aug=False,
                                    prob=valid_prob,
                                    shuffle=True)
 
-    # 训练
-    callbacks = [EarlyStopping(monitor='val_loss', patience=10),
-                 CSVLogger(os.path.join(model_data, 'train_log.csv')),
-                 ModelCheckpoint(filepath="./model_data/model.{epoch:02d}-{val_loss:.2f}.h5",
-                                 save_best_only=False,
-                                 period=2)]
+    """
+    训练：
+    1. warmup
+    2. 正式训练
+    """
+    # warmup
+    model.fit_generator(train_data_gen,
+                        epochs=warmup_epochs,
+                        validation_data=valid_data_gen,
+                        workers=train_workers)
+    # 正式训练
+    callbacks = [  # EarlyStopping(monitor='val_loss', patience=10),
+        CSVLogger(os.path.join(model_data, 'train_log.csv')),
+        ModelCheckpoint(filepath="./model_data/model.{epoch:02d}-{val_loss:.8f}.h5",
+                        save_best_only=False,
+                        period=2)]
 
-    model.fit_generator(train_data_gen, epochs=200, validation_data=valid_data_gen, workers=1, use_multiprocessing=True,
+    model.fit_generator(train_data_gen,
+                        epochs=200,
+                        validation_data=valid_data_gen,
+                        workers=train_workers,
+                        use_multiprocessing=True,
                         callbacks=callbacks)
 
 
