9ab8812881ed2c2ec45b159a27595780af5a334a
huangchengjun <huangchengjun@eastmoney.com>
2023-10-28 17:46:41 +0800

    feat: 使用fastapi提供接口服务

---
+++ apiServer.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from fastapi import FastAPI, UploadFile, File
+import numpy as np
+import config
+import cv2
+from keras import Model
+from keras.models import load_model
+
+from util.imageProcess import imgProcessNorm
+from util.modelUtils import word_acc
+from util import labelProcess
+
+app = FastAPI()
+
+model_path = r"E:\work\pythonProject\CAPTCHA_full\model_data\VGG\checkpoints\VGG.h5"
+model: Model = load_model(model_path, custom_objects={"word_acc": word_acc})
+
+
+@app.post("/captcha/recognition/")
+async def upload_images(files: List[UploadFile] = File(...)):
+    # 处理接收到的图片文件
+    images = []
+    for file in files:
+        content = await file.read()
+        image_array = np.frombuffer(content, np.uint8)
+        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        image = imgProcessNorm(img=image, shape=config.Model.img_shape)
+        images.append(image)
+
+    predict = model.predict(np.array(images))
+    pred = labelProcess.decode_predict(predict)
+    print('预测结果：' + str(pred))
+    return {"message": "Images uploaded successfully", "images": pred}
